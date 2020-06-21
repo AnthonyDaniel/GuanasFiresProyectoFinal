5ff550f7f3f93c348f13789398e46e291c67791f
kelinn <kasvillarreal@gmail.com>
2020-06-21 00:22:12 -0600

    notificaciones

---
+++ lib/services/notifications.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:async';
+
+void main() => runApp(new Notifications());
+class Notifications extends StatefulWidget {
+  Notifications({Key key}) : super(key: key);
+
+  @override
+  _NotificationsState createState() => _NotificationsState();
+}
+
+class _NotificationsState extends State<Notifications> {
+String textValue = 'Hello World !';
+  FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      new FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+
+    var android = new AndroidInitializationSettings('mipmap/ic_launcher');
+    var ios = new IOSInitializationSettings();
+    var platform = new InitializationSettings(android, ios);
+    flutterLocalNotificationsPlugin.initialize(platform);
+
+    firebaseMessaging.configure(
+      onLaunch: (Map<String, dynamic> msg) {
+        print(" onLaunch called ${(msg)}");
+      },
+      onResume: (Map<String, dynamic> msg) {
+        print(" onResume called ${(msg)}");
+      },
+      onMessage: (Map<String, dynamic> msg) {
+        showNotification(msg);
+        print(" onMessage called ${(msg)}");
+      },
+    );
+    firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, alert: true, badge: true));
+    firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings setting) {
+      print('IOS Setting Registed');
+    });
+   
+  }
+
+  showNotification(Map<String, dynamic> msg) async {
+    var android = new AndroidNotificationDetails(
+      'sdffds dsffds',
+      "CHANNLE NAME",
+      "channelDescription",
+    );
+    var iOS = new IOSNotificationDetails();
+    var platform = new NotificationDetails(android, iOS);
+    await flutterLocalNotificationsPlugin.show(
+        0, "Nuevo incendio", "Se ha reporta un incendio nuevo", platform);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+        return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Push Notification'),
+        ),
+        body: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text(
+                textValue,
+              )
+            ],
+          ),
+        ),
+    );
+  }
+}
--- triggers/functions/index.js
+++ triggers/functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+    exports.helloWorld = functions.database.ref('fires/{guanasfiresId}').onWrite((change, context)=> {
+        var token= 'dOPqDj1uSgCMHaV1cQsqDF:APA91bFBOsKkYVFFc9Ki9rtQHGSNJpioQeIrSZcE67ijE7fDz5tIRKI8cC7glA88bieEAo1F6yrjud_pzN1vtovXzPtkrTUiA_XweFT8owNnEB1k6chhcdhcBZFv0owz_RjgTIRLsRTN';
+        const payload = {
+            notification:{
+                title : 'Message from Cloud',
+                body : 'This is your body',
+                badge : '1',
+                sound : 'default'
+            }
+        };
+        console.log(change.after.val().email);
+        return admin.messaging().sendToDevice(token,payload);
+        
+    });
